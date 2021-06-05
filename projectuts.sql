c4e0d5851f6c10edb636ca7c7d055a4256402cf2
Boby Hartanto <85204030+BobyHart4488@users.noreply.github.com>
2021-06-05 21:33:05 +0700

    Editing Skrip SQL
    "Selamat berjuang"

--- projectuts.sql
+++ projectuts.sql
@@ -12,34 +12,15 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `projectuts`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `film`
---
-
-CREATE TABLE `film` (
+CREATE TABLE film (
   `no` int(11) NOT NULL,
   `image` varchar(120) NOT NULL,
   `title` varchar(120) NOT NULL,
   `sinopsis` longtext NOT NULL,
-  `iframe` varchar(12000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `iframe` varchar(12000) NOT NULL);
 
---
--- Dumping data for table `film`
---
 
-INSERT INTO `film` (`no`, `image`, `title`, `sinopsis`, `iframe`) VALUES
+INSERT INTO film (`no`, `image`, `title`, `sinopsis`, `iframe`) VALUES
 (2, 'thewitcherliveaction.jpg', 'The Witcher ', 'Geralt of Rivia, a solitary monster hunter, struggles to find his place in a world where people often prove more wicked than beasts.', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ndl1W4ltcmg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
 (3, 'Conjuring_poster.jpg', 'The Conjuring', 'Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a dark presence in their farmhouse.', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/k10ETZ41q5o\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
 (4, 'MV5BZDk5MTcxZmItYWM2Ny00NjIwLWE2YWQtYjczMWFiZThjN2JkXkEyXkFqcGdeQXVyNDQyNzIwNDQ@._V1_.jpg', 'Attack on Titan Part 1 (2015)', 'In a world where giant humanoid Titans prey on humans, Eren joins the scouting legion to get revenge on the monsters who killed everyone in his town', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/hk7ltMVX2wU\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
@@ -50,13 +31,8 @@ INSERT INTO `film` (`no`, `image`, `title`, `sinopsis`, `iframe`) VALUES
 (13, '2f36ed19d69c1cc044aab03f388f5082.jpg', 'The Conjuring 2', 'Ed and Lorraine Warren travel to North London to help a single mother raising four children alone in a house plagued by a supernatural spirit.', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/VFsmuRPClr4\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
 (15, 'MV5BOWNkODY2NGYtNGQ1NS00NjEzLTllMjgtY2YzMDZjNjFiN2MzXkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_.jpg', 'The Human Centipede (First Sequence)', 'A mad scientist kidnaps and mutilates a trio of tourists in order to reassemble them into a human centipede,', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/glfBurdSUS8\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
+CREATE TABLE user (
   `no` int(120) NOT NULL,
   `first_name` varchar(120) NOT NULL,
   `last_name` varchar(120) NOT NULL,
@@ -65,11 +41,8 @@ CREATE TABLE `user` (
   `password` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `user`
---
 
-INSERT INTO `user` (`no`, `first_name`, `last_name`, `username`, `email`, `password`) VALUES
+INSERT INTO user (`no`, `first_name`, `last_name`, `username`, `email`, `password`) VALUES
 (1, 'udin', 'simanjuntak', 'udin123', 'udin@gmil.com', '10203200'),
 (3, 'muhammad', 'udin', 'udin10', 'udin@gmil.co.id', '123'),
 (5, 'xeno', 'arges', 'arges', 'arges@gmail.com', 'arges123'),
@@ -82,39 +55,11 @@ INSERT INTO `user` (`no`, `first_name`, `last_name`, `username`, `email`, `passw
 (31, 'muhammad', 'razaq', 'razaq', 'razaq13@gmail .com', '123'),
 (32, 'Muhammad', 'Fajri Afriyansyah', 'Muhammad Fajri Afriyansyah', 'fajri20ti@mahasiswa.pcr.ac.id', '10010');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `film`
---
-ALTER TABLE `film`
-  ADD PRIMARY KEY (`no`);
+ALTER TABLE film ADD PRIMARY KEY (`no`);
 
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`no`);
+ALTER TABLE user ADD PRIMARY KEY (`no`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+ALTER TABLE film MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
---
--- AUTO_INCREMENT for table `film`
---
-ALTER TABLE `film`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `no` int(120) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE user MODIFY `no` int(120) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
